db90280e6adff447911d3c138459caed1dc76ffb
Pmarva <marvin002msn.com>
2016-02-24 16:24:58 +0200

    Kodutöö number 3

--- src/homework/homework3/PictureToAc.java
+++ src/homework/homework3/PictureToAc.java
@@ -22,7 +22,7 @@ public class PictureToAc extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        double height = 800; // iamge height, automatic aspect ratio.. set 0 for original
+        double height = 250; // iamge height, automatic aspect ratio.. set 0 for original
         String imageLocation = "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/12/2015/03/Canon-EF-100-400mm-f4.5-5.6L-IS-II-sample-image-1.jpg"; //image location URL, or file path in pc
 
         Image image = new Image(imageLocation, 0, height, true, false);
